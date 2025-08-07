b26291b1b0e7fad33c2c0ba78e601af5189ae3c7
Hossein Zahed <hzahed@outlook.com>
2025-08-07 14:18:09 +0200

    feat: Add PostgreSQL ping service and update VS Code configurations

--- src/crawlers/function_app.py
+++ src/crawlers/function_app.py
@@ -1,21 +1,23 @@
-import os
 import azure.functions as func
 import logging
 from dotenv import load_dotenv
 from cosmos_db_service import CosmosDBService
 from foundry_service import FoundryService
+from psql_ping_service import PostgresPingService
 
 # Load environment variables from .env file
 load_dotenv(override=True)
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("azure.functions")
+#logging.basicConfig(level=logging.INFO)
+#logging.getLogger("azure.functions")
+#logging.getLogger("azure.cosmos").setLevel(logging.CRITICAL)
 
 app = func.FunctionApp()
 
 cosmos_db_service = CosmosDBService()
 foundry_service = FoundryService()
+postgres_ping_service = PostgresPingService()
 
 
 @app.timer_trigger(schedule="0 0 0 1 1 *",  # Run every 1 day
@@ -23,12 +25,12 @@ foundry_service = FoundryService()
                    run_on_startup=False,
                    use_monitor=False)
 def github_crawler_func(timer_request: func.TimerRequest) -> None:
-    logger.info('GitHub crawler function started.')
+    logging.info('GitHub crawler function started.')
     from github_crawler import GitHubCrawler
     github_crawler = GitHubCrawler(cosmos_db_service=cosmos_db_service,
                                    foundry_service=foundry_service)
     github_crawler.run()
-    logger.info('GitHub crawler function finished.')
+    logging.info('GitHub crawler function finished.')
 
 
 @app.timer_trigger(schedule="0 0 0 * * *",  # Run every 1 day
@@ -36,12 +38,12 @@ def github_crawler_func(timer_request: func.TimerRequest) -> None:
                    run_on_startup=True,
                    use_monitor=False)
 def blogs_crawler_func(timer_request: func.TimerRequest) -> None:
-    logger.info('Blogs crawler function started.')
+    logging.info('Blogs crawler function started.')
     from blogs_crawler import BlogsCrawler
     blogs_crawler = BlogsCrawler(cosmos_db_service=cosmos_db_service,
                                  foundry_service=foundry_service)
     blogs_crawler.run()
-    logger.info('Blogs crawler function finished.')
+    logging.info('Blogs crawler function finished.')
 
 
 @app.timer_trigger(schedule="0 0 0 1 1 *",  # Run every year on January 1st
@@ -49,20 +51,19 @@ def blogs_crawler_func(timer_request: func.TimerRequest) -> None:
                    run_on_startup=False,
                    use_monitor=False)
 def seismic_crawler_func(timer_request: func.TimerRequest) -> None:
-    logger.info('Seismic crawler function started.')
+    logging.info('Seismic crawler function started.')
     from seismic_crawler import SeismicCrawler
     seismic_crawler = SeismicCrawler(cosmos_db_service=cosmos_db_service,
                                      foundry_service=foundry_service)
     seismic_crawler.run()
-    logger.info('Seismic crawler function finished.')
+    logging.info('Seismic crawler function finished.')
 
-@app.timer_trigger(schedule="0 */5 * * * *", 
-                   arg_name="timer_request", 
-                   run_on_startup=True, 
-                   use_monitor=False) 
+
+@app.timer_trigger(schedule="0 */5 * * * *",
+                   arg_name="timer_request",
+                   run_on_startup=True,
+                   use_monitor=False)
 def ping_psql_func(timer_request: func.TimerRequest) -> None:
-    if timer_request.past_due:
-        logging.info('The timer is past due!')
-    endpoint = os.environ.get('COSMOSDB_ENDPOINT')
-    logging.info(endpoint)
-    logging.info('Python timer trigger function executed.')
+    logging.info('Ping PostgreSQL function started.')
+    postgres_ping_service.run()
+    logging.info('Ping PostgreSQL function finished.')
